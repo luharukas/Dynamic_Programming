478d7099c3b887c45caad8416000e8f323e6d3dd
Shubham Luharuka <luharukashubham4@gmail.com>
2021-09-02 12:00:00 +0530

    6 files updates

--- Longest_Common_subSequence.py
+++ Longest_Common_subSequence.py
@@ -1,25 +1,26 @@
-# Find the length of Largest Common Subsequence in the given array
+
+# Find the length of Largest Common Subsequence in the given String
 #This program is written in Python language 
 #Time complexity of this program is O(N^2)
-#Space Complexity of this program is O(n)
-def Longest_Common_sub_Sequence(nums):
-    n=len(nums)
-    result_array=[1]*n
-    for i in range(1,n):
-        for j in range(0,i):
-            if nums[i]>nums[j]:
-                result_array[i]=max(1+result_array[j],result_array[i])
-
+#Space Complexity of this program is O(N^2)
+def longest_common_subsequence(string1,string2):
+    m=len(string1)
+    n=len(string2)
+    arr=[[0 for i in range(m+1)] for j in range(n+1)]
     
-    maximum=0
-    for i in result_array:
-        maximum=max(maximum,i)
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if string1[j-1]==string2[i-1]:
+                arr[i][j]=1+arr[i-1][j-1]
+            else:
+                arr[i][j]=max(arr[i-1][j],arr[i][j-1])
 
-    return maximum
+
+    return arr[-1][-1]
+
+
 
 if __name__=='__main__':
-    array=input("Enter array").split(',')
-    array=list(map(int,array))
-    length=Longest_Common_sub_Sequence(array)
-    print('Length of largest common subsequence is ')
-    print(length)
+    input_string1=list(input("Enter string 1: "))
+    input_string2=list(input("Enter string 2: "))
+    longest_common_subsequence(input_string1,input_string2)
